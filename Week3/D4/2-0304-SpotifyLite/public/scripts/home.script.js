d536b2b9afce0f7cf21e5f7228417d3be4300468
albert ahn <albertfromsd@gmail.com>
2022-12-09 02:45:07 -0800

    W3D4-Q2: Initial setup

--- Week3/D4/2-0304-SpotifyLite/public/scripts/home.script.js
+++ Week3/D4/2-0304-SpotifyLite/public/scripts/home.script.js
@@ -7,17 +7,3 @@ window.onbeforeunload = function(e) {
     localStorage.setItem('scrollpos', window.scrollY);
 };
 
-function toggleRow(idx) {
-    const infoRow = document.getElementById(`info-${idx}`);
-    infoRow.classList.toggle("hidden");
-    const editRow = document.getElementById(`edit-${idx}`);
-    editRow.classList.toggle("hidden");
-}
-
-async function createTodoList() {
-    try {
-        await fetch('/todo/seed/todolist', { method: "POST" });
-    } catch(e) {
-        console.log( 'error in creating todo list', {e} );
-    }
-}
